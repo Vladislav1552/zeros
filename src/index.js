eee44f439420b9c019a7eab49666ae174ae87f82
Vladislav1552 <vladiskav333@gmail.com>
2018-03-01 18:43:10 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+
+  
+  var n = 5, end = 0;
+  while (n < number){
+        end += Math.floor(number/n);
+          n *= 5;
+  }
+      return  end;
+  }
+
